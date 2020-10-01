6bdd3f2fbc9d012042b08e762c2e571ff03a4ae9
kadirsalvarci <67653353+kadirsalvarci@users.noreply.github.com>
2020-10-01 13:28:52 +0200

    no_debug

--- aws/projects/001-roman-numerals-converter/app.py
+++ aws/projects/001-roman-numerals-converter/app.py
@@ -29,5 +29,5 @@ def index ():
         return render_template ("index.html", developer_name = developer_name)
 
 if __name__ == "__main__":
-    app.run(debug = True)
-    #app.run (host = "0.0.0.0", port=80)
+    #app.run(debug = True)
+    app.run (host = "0.0.0.0", port=80)
