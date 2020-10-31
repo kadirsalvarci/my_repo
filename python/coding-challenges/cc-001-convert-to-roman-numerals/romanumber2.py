72fb13480213e32ed80efd7008b8120fbea1b2d9
kadirsalvarci <kadir.salvarci@gmail.com>
2020-10-31 20:52:22 +0100

    okkk

--- python/coding-challenges/cc-001-convert-to-roman-numerals/romanumber2.py
+++ python/coding-challenges/cc-001-convert-to-roman-numerals/romanumber2.py
@@ -1,4 +1,4 @@
-def InttoRoman(number):
+pythondef InttoRoman(number):
     romandict = {'M':1000, 'CM':900, 'D':500, 'CD':400, 'C':100, 'XC':90, 'L':50, 'XL':40, 'X':10, 'IX':9, 'V': 5, 'IV':4, 'I':1}
     if (not number.isdigit()) or ((int(number) > 3999) or (int(number) < 1)):
         return "Not Valid Input !!!"
